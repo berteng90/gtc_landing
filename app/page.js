f7c70aba7953874fb59df5c65764159806be4682
berteng90 <berteng90@gmail.com>
2023-09-29 10:30:37 +0800

    Added a Statistics Section

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import { NavBar } from "@/components/NavBar";
 import { Hero } from "@/components/Hero";
 import { Footer } from "@/components/Footer";
 import { Branches } from "@/components/Branches";
+import { Statistics } from "@/components/Statistics";
 export default function Home() {
   return (
     <main className="w-full h-full overflow-hidden">
       <NavBar />
       <Hero />
       <Branches />
+      <Statistics />
       <Footer />
     </main>
   );
