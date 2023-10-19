11ed23acfc54a849ce602e0cfa9304eccac03ceb
berteng90 <berteng90@gmail.com>
2023-10-19 15:11:00 +0800

    Reverted Registration Page to Previous Version

--- app/registration/page.js
+++ app/registration/page.js
@@ -2,8 +2,8 @@ import { Registration } from "@/components/Registration";
 
 export default function Page() {
   return (
-    <>
+    <main className="overflowe-hidden w-full h-screen">
       <Registration />
-    </>
+    </main>
   );
 }
